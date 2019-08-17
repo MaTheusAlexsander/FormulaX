ba07bbfe6563b62c085566d83fc314d50d0d60fe
MaTheusAlexsander <51827175+MaTheusAlexsander@users.noreply.github.com>
2019-08-17 10:31:21 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ socketio = SocketIO(app)
 CORS(app)
 
 estado = {
-    "LED01" : 0,
-    "LED02" : 0
+    "Q1":0,
+    "Q2":0
 }
+change=0
     
 
 
@@ -21,24 +22,43 @@ def rota_inicial():
     
 @app.route("/upload", methods = ["POST"])
 def rota_upload():
-    mensagem = request.get_json()
-    socketio.emit("atualiza",mensagem)
+    global estado
+    estado = request.get_json()
+    socketio.emit("atualiza",estado)
     return "200"
 
 @app.route("/download", methods=['GET'])
 def rota_download():
     global estado
-    return json.dumps(estado)
+    if change==1:
+        change = 0
+        return json.dumps(estado)
+    return "1"
 
-@socketio.on("ligarLed")
-def ligar_led():
-    global estado
-    estado["LED01"] = 1
 
-@socketio.on("desligarLed")
-def desligar_led():
-    global estado
-    estado["LED01"] = 0
+@socketio.on("ligarQ1")
+def ligar_Q1():
+    global estado, change
+    change=1
+    estado["Q1"] = 1
 
+@socketio.on("desligarQ1")
+def desligar_Q1():
+    global estado, change
+    change=1
+    estado["Q1"] = 0
+'''
+@socketio.on("definirT1")
+def definir_T1():
+    global estado, change
+    change=1
+    estado["T1"] = tempo1
+
+@socketio.on("definirT2")
+def definir_T2():
+    global estado, change
+    change=1
+    estado["T2"] = tempo1
+'''
 if __name__ == "__main__":
     app.run("0.0.0.0",port=8080)
